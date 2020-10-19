62c3347a2fdc80edffb0e02b431aa5e174a1e98c
LGordy20 <liam.gordy@patelhs.org>
2020-10-19 15:49:53 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,14 @@
+class Main {
+  public static void main(String[] args) {
+    int myAge = 14;
+    int myBirthday = 18;
+    int todaysDate = 26;
+
+    if( myAge == todaysDate)
+    {
+      System.out.println("I am" + myAge +"years old.");
+    }else{
+      System.out.println("I am still " + myAge +" years old.");
+    }
+  }
+}
